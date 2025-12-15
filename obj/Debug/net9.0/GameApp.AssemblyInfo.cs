466f736ffa51aae31da068f04ae12bca5e1e76dc
Antonio Melendez <mel23001@byui.edu>
2025-12-15 14:59:04 -0600

    website

--- obj/Debug/net9.0/GameApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d369a1a3e5adf83538e1bd7661ad5df6a6ce59f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42d47f1b14af86af32ae057b0db3c8c5646ed34")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
