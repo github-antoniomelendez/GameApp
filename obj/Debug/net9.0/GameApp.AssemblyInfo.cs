e9aa3844cac95ce748e2cdf03fb2060e194cdf5e
Antonio Melendez <mel23001@byui.edu>
2025-12-15 12:38:55 -0600

    hangman fix

--- obj/Debug/net9.0/GameApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66dd263ee15b54f761d4368ec8bf71ee38e84fe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31763dac9489d9267319938ea92b79d4a2f1436d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
